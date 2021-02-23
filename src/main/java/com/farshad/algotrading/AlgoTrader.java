e06ed42f1abd7535f7fb77bbdf91b30e74aea28c
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-02-23 12:00:22 +0000

    updates build

--- src/main/java/com/farshad/algotrading/AlgoTrader.java
+++ src/main/java/com/farshad/algotrading/AlgoTrader.java
@@ -1,4 +1,14 @@
 package com.farshad.algotrading;
 
 import com.farshad.algotrading.backtesting.ATSBackTest;
-import com.farshad.algotrading.backtesting.Ba
+import com.farshad.algotrading.backtesting.BackTest;
+import com.farshad.algotrading.backtesting.FetchingForexBacktest;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+
+/**
+ * @author Farshad Noravesh
+ */
+public c
