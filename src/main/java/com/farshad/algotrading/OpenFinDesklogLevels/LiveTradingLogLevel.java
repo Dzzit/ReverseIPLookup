4ae3d0c8cf774a9ff235db7ece63ee1784d0a5c9
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-02-28 08:58:05 +0000

    fix assets

--- src/main/java/com/farshad/algotrading/OpenFinDesklogLevels/LiveTradingLogLevel.java
+++ src/main/java/com/farshad/algotrading/OpenFinDesklogLevels/LiveTradingLogLevel.java
@@ -1,1 +1,10 @@
-pack
+package com.farshad.algotrading.OpenFinDesklogLevels;
+
+
+import org.apache.log4j.Level;
+
+
+
+public class LiveTradingLogLevel  extends Level {
+
+    public static final int PROCESS_IN
