dcfa2e360c29ab6715ac77276f0fe947b926acdf
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-03-01 07:55:06 +0000

    USB: fix PS3 EHCI systems

--- src/main/java/com/farshad/algotrading/OpenFinDesklogLevels/LiveTradingLogLevel.java
+++ src/main/java/com/farshad/algotrading/OpenFinDesklogLevels/LiveTradingLogLevel.java
@@ -7,4 +7,9 @@ import org.apache.log4j.Level;
 
 public class LiveTradingLogLevel  extends Level {
 
-    public static final int PROCESS_IN
+    public static final int PROCESS_INT = DEBUG_INT -10;
+    public static final int SOCKETUTIL_INT = INFO_INT -11;
+    public static final int SIGNALS_INT = WARN_INT +12;
+
+
+    public static final Lev
