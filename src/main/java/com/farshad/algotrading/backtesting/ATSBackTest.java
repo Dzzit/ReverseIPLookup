4a68fa2e8e58527f6947f690b592b60c1cbdf04f
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-03-02 19:18:07 +0000

    remove tab

--- src/main/java/com/farshad/algotrading/backtesting/ATSBackTest.java
+++ src/main/java/com/farshad/algotrading/backtesting/ATSBackTest.java
@@ -1,4 +1,7 @@
 package com.farshad.algotrading.backtesting;
 
 import com.farshad.algotrading.core.AlternatingTrendSmoothing.ATS;
-import com.farshad.algotrading.backtesting.influxDbCRUD.InfluxDbMana
+import com.farshad.algotrading.backtesting.influxDbCRUD.InfluxDbManager;
+import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.PriceTime;
+import com.farshad.algotrading.core.Symbol;
+import com.farshad.algotrading.core.openFinDeskStore.influxd
