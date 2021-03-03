9430269562a672e8132e11e3b0bfd2b007e0c589
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-03-03 14:17:44 +0000

    display fix

--- src/main/java/com/farshad/algotrading/backtesting/ATSBackTest.java
+++ src/main/java/com/farshad/algotrading/backtesting/ATSBackTest.java
@@ -4,4 +4,13 @@ import com.farshad.algotrading.core.AlternatingTrendSmoothing.ATS;
 import com.farshad.algotrading.backtesting.influxDbCRUD.InfluxDbManager;
 import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.PriceTime;
 import com.farshad.algotrading.core.Symbol;
-import com.farshad.algotrading.core.openFinDeskStore.influxd
+import com.farshad.algotrading.core.openFinDeskStore.influxdb.InfluxDB;
+import org.apache.log4j.Logger;
+import org.ta4j.core.TimeSeries;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * @
