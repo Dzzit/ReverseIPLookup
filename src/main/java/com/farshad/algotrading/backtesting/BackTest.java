be936b51c14f8940a581341811ebd9ff8397a2a3
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-03-15 20:34:12 +0000

    improved docstrings

--- src/main/java/com/farshad/algotrading/backtesting/BackTest.java
+++ src/main/java/com/farshad/algotrading/backtesting/BackTest.java
@@ -1,4 +1,11 @@
 package com.farshad.algotrading.backtesting;
 
 import com.farshad.algotrading.core.sockets.SocketUtil;
-import com.farshad.algotrading.core.openFinDeskSt
+import com.farshad.algotrading.core.openFinDeskStore.csv.stocksInputData.OHLCData;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * @author Farshad 
