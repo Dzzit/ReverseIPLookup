eb535c0ae0f6178a9fbbf8d794b8ce60434bf02e
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-04-16 20:45:58 +0000

    h8300: Add missing RCU idle APIs on idle loop

--- src/main/java/com/farshad/algotrading/backtesting/FibonacciBackTest.java
+++ src/main/java/com/farshad/algotrading/backtesting/FibonacciBackTest.java
@@ -1,4 +1,7 @@
 package com.farshad.algotrading.backtesting;
 
 
-import com.farshad.algotrading.backtesting.influxDbCRUD.
+import com.farshad.algotrading.backtesting.influxDbCRUD.InfluxDbManager;
+import com.farshad.algotrading.backtesting.measurements.AUDUSDCandlePoint;
+import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.PriceTime;
+import 
