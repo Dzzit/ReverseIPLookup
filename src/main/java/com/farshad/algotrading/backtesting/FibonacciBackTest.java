4d41a2c939cb227f4f8b5772a0f5d1022b68dac5
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-04-17 15:37:46 +0000

    update links

--- src/main/java/com/farshad/algotrading/backtesting/FibonacciBackTest.java
+++ src/main/java/com/farshad/algotrading/backtesting/FibonacciBackTest.java
@@ -7,3 +7,6 @@ import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.P
 import com.farshad.algotrading.core.AlternatingTrendSmoothing.ATS;
 import com.farshad.algotrading.core.trendRangeAndWave.Trend;
 import com.farshad.algotrading.core.trendRangeAndWave.TrendGenerator;
+import com.farshad.algotrading.core.trendRangeAndWave.Wave;
+import com.farshad.algotrading.core.trendRangeAndWave.WaveGenerator;
+import com.farshad.algotrading.core.fibonacciCore.Fibona
