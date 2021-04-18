7fda1392e2e2db142e3affd37ec7481d48b1370d
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-04-18 21:03:27 +0000

    fixed baseurl

--- src/main/java/com/farshad/algotrading/backtesting/FibonacciBackTest.java
+++ src/main/java/com/farshad/algotrading/backtesting/FibonacciBackTest.java
@@ -9,4 +9,13 @@ import com.farshad.algotrading.core.trendRangeAndWave.Trend;
 import com.farshad.algotrading.core.trendRangeAndWave.TrendGenerator;
 import com.farshad.algotrading.core.trendRangeAndWave.Wave;
 import com.farshad.algotrading.core.trendRangeAndWave.WaveGenerator;
-import com.farshad.algotrading.core.fibonacciCore.Fibona
+import com.farshad.algotrading.core.fibonacciCore.Fibonacci;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * @author Farshad Noravesh
+ */
+public class FibonacciBackTest extends Back
