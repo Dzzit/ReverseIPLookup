95335a658995b128f25afd877a36ebb04d3f34e0
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-05-13 10:50:27 +0000

    Updated the google-cloud-bigquery feedstock.

--- src/main/java/com/farshad/algotrading/backtesting/HMM/BackwardAlgorithm.java
+++ src/main/java/com/farshad/algotrading/backtesting/HMM/BackwardAlgorithm.java
@@ -57,4 +57,5 @@ public class BackwardAlgorithm {
 
     public double beta01(double observation){
         double pxConditionedOnaAtSpecificObservation=(1/Math.sqrt((2*Math.PI*expectationMaximization.getVarianceOfa()))) * Math.exp(-Math.pow(observation - expectationMaximization.getMeanOfa(), 2) / (2 * expectationMaximization.getVarianceOfa()));
-        double pxConditionedOnbAtSpecificObservation=(1/Math.sqrt((2*Math.PI*expectationMaximizat
+        double pxConditionedOnbAtSpecificObservation=(1/Math.sqrt((2*Math.PI*expectationMaximization.getVarianceOfb()))) * Math.exp(-Math.pow(observation - expectationMaximization.getMeanOfb(), 2) / (2 * expectationMaximization.getVarianceOfb()));
+        beta01=1*pxConditionedOnaAtSpecificObservation
