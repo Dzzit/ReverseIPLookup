ba42b3d6948189559f140ab83f2469447e837058
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-05-13 17:12:19 +0000

    Check travis

--- src/main/java/com/farshad/algotrading/backtesting/HMM/BackwardAlgorithm.java
+++ src/main/java/com/farshad/algotrading/backtesting/HMM/BackwardAlgorithm.java
@@ -58,4 +58,10 @@ public class BackwardAlgorithm {
     public double beta01(double observation){
         double pxConditionedOnaAtSpecificObservation=(1/Math.sqrt((2*Math.PI*expectationMaximization.getVarianceOfa()))) * Math.exp(-Math.pow(observation - expectationMaximization.getMeanOfa(), 2) / (2 * expectationMaximization.getVarianceOfa()));
         double pxConditionedOnbAtSpecificObservation=(1/Math.sqrt((2*Math.PI*expectationMaximization.getVarianceOfb()))) * Math.exp(-Math.pow(observation - expectationMaximization.getMeanOfb(), 2) / (2 * expectationMaximization.getVarianceOfb()));
-        beta01=1*pxConditionedOnaAtSpecificObservation
+        beta01=1*pxConditionedOnaAtSpecificObservation*transitionProbability[0][1]
+              +1*pxConditionedOnbAtSpecificObservation*transitionProbability[1][1];
+        return beta01;
+    }
+
+
+    private void calculateEmissionProbabili
