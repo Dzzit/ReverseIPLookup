c703bd06cfeeea76e3b7ed70bcbd5924c7fa1025
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-05-16 15:41:47 +0000

    Remove line break

--- src/main/java/com/farshad/algotrading/backtesting/HMM/RegimeAnalyzer.java
+++ src/main/java/com/farshad/algotrading/backtesting/HMM/RegimeAnalyzer.java
@@ -1,1 +1,5 @@
-package com.farshad.algotrading.backtesti
+package com.farshad.algotrading.backtesting.HMM;
+
+import com.farshad.algotrading.backtesting.measurements.AUDUSDCandlePoint;
+import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.PercentOfChange;
+import com.far
