6f8e70ae006c9cb7513e79f64b1efc68725502d3
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-05-18 13:51:40 +0000

    improve docstring

--- src/main/java/com/farshad/algotrading/backtesting/HMM/RegimeAnalyzer.java
+++ src/main/java/com/farshad/algotrading/backtesting/HMM/RegimeAnalyzer.java
@@ -2,4 +2,10 @@ package com.farshad.algotrading.backtesting.HMM;
 
 import com.farshad.algotrading.backtesting.measurements.AUDUSDCandlePoint;
 import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.PercentOfChange;
-import com.far
+import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.Return;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegimeAnalyzer {
