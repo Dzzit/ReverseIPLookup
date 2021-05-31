1ab537a5de6287349b68b4ad6d2d52354fd7d8e0
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-05-31 17:16:00 +0000

    Angular

--- src/main/java/com/farshad/algotrading/backtesting/HMMBackTest.java
+++ src/main/java/com/farshad/algotrading/backtesting/HMMBackTest.java
@@ -1,3 +1,6 @@
 package com.farshad.algotrading.backtesting;
 
-import com.farshad.algotrading.backtesting.HMM.Backw
+import com.farshad.algotrading.backtesting.HMM.BackwardAlgorithm;
+import com.farshad.algotrading.backtesting.HMM.RegimeAnalyzer;
+import com.farshad.algotrading.backtesting.measurements.AUDUSDCandlePoint;
+import com.farshad.algotrading.backtesting.influxDbCRU
