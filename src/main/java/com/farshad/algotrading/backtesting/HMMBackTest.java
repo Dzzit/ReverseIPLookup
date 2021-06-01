0d84446dc125f4fbe945b781bee89f35cf750b26
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-06-01 08:36:30 +0000

    Update zc.sh

--- src/main/java/com/farshad/algotrading/backtesting/HMMBackTest.java
+++ src/main/java/com/farshad/algotrading/backtesting/HMMBackTest.java
@@ -3,4 +3,6 @@ package com.farshad.algotrading.backtesting;
 import com.farshad.algotrading.backtesting.HMM.BackwardAlgorithm;
 import com.farshad.algotrading.backtesting.HMM.RegimeAnalyzer;
 import com.farshad.algotrading.backtesting.measurements.AUDUSDCandlePoint;
-import com.farshad.algotrading.backtesting.influxDbCRU
+import com.farshad.algotrading.backtesting.influxDbCRUD.InfluxDbManager;
+import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.PercentOfChange;
+import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasure
