d1e436ca120827dbc47fe7f6ea14120f6b868b76
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-06-01 08:41:13 +0000

    prep for 1.1.0 release

--- src/main/java/com/farshad/algotrading/backtesting/HMMBackTest.java
+++ src/main/java/com/farshad/algotrading/backtesting/HMMBackTest.java
@@ -5,4 +5,11 @@ import com.farshad.algotrading.backtesting.HMM.RegimeAnalyzer;
 import com.farshad.algotrading.backtesting.measurements.AUDUSDCandlePoint;
 import com.farshad.algotrading.backtesting.influxDbCRUD.InfluxDbManager;
 import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.PercentOfChange;
-import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasure
+import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.Return;
+import com.farshad.algotrading.core.openFinDeskStore.csv.stocksInputData.OHLCData;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Lis
