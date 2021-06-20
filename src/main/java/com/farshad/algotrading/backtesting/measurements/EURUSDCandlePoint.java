606a8c1a4f846d118fbdbcb2f6e024d8723ea8c0
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-06-20 21:57:16 +0000

    333

--- src/main/java/com/farshad/algotrading/backtesting/measurements/EURUSDCandlePoint.java
+++ src/main/java/com/farshad/algotrading/backtesting/measurements/EURUSDCandlePoint.java
@@ -3,4 +3,14 @@ package com.farshad.algotrading.backtesting.measurements;
 
 import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.TimeSeriesPoint;
 import org.influxdb.annotation.Column;
-import org.influxdb.annotation.Measurement
+import org.influxdb.annotation.Measurement;
+
+@Measurement(name = "EURUSD")
+ public class EURUSDCandlePoint extends TimeSeriesPoint {
+
+
+    @Column(name = "open")
+    private double open;
+
+    @Column(name = "high")
+    private do
