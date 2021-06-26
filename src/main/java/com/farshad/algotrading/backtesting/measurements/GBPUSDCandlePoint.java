eab7a38779890afcbe17353d9dcbe40de5d11e5a
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-06-26 07:20:06 +0000

    More documentation cleanup

--- src/main/java/com/farshad/algotrading/backtesting/measurements/GBPUSDCandlePoint.java
+++ src/main/java/com/farshad/algotrading/backtesting/measurements/GBPUSDCandlePoint.java
@@ -1,4 +1,9 @@
 package com.farshad.algotrading.backtesting.measurements;
 
 
-import com.farshad.algotrading.backtesting.measurements.timeSeri
+import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.TimeSeriesPoint;
+import org.influxdb.annotation.Column;
+import org.influxdb.annotation.Measurement;
+
+@Measurement(name = "GBPUSD")
+public class GBPUSDCandlePoint extends TimeSeriesPo
