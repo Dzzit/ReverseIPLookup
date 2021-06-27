7a3c7105bfd0ca6e874ba6a4f61f57496bd1c9c5
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-06-27 18:28:49 +0000

    Fixing URL

--- src/main/java/com/farshad/algotrading/backtesting/measurements/timeSeriesMeasurements/PercentOfChange.java
+++ src/main/java/com/farshad/algotrading/backtesting/measurements/timeSeriesMeasurements/PercentOfChange.java
@@ -1,1 +1,8 @@
-pac
+package com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements;
+
+
+import org.influxdb.annotation.Column;
+import org.influxdb.annotation.Measurement;
+
+@Measurement(name = "EURUSDChangeInPercent")
+p
