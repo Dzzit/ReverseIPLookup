a745b7ee110a1faefd254c02b079a2bb7dc2a3c6
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-07-02 18:21:00 +0000

    v0.4.11

--- src/main/java/com/farshad/algotrading/backtesting/measurements/timeSeriesMeasurements/PriceTime.java
+++ src/main/java/com/farshad/algotrading/backtesting/measurements/timeSeriesMeasurements/PriceTime.java
@@ -1,4 +1,16 @@
 package com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements;
 
  import org.influxdb.annotation.Column;
- import org.i
+ import org.influxdb.annotation.Measurement;
+
+ import java.time.Instant;
+
+
+@Measurement(name = "priceTime")
+public class PriceTime extends TimeSeriesPoint {
+
+
+    @Column(name = "price")
+    private double price;
+
+    privat
