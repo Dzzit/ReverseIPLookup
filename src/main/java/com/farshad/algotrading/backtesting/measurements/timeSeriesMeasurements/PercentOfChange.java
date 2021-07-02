c6b86e1e4e599134ae2b252ff3095e91456a78b6
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-07-02 14:25:37 +0000

    squash npe

--- src/main/java/com/farshad/algotrading/backtesting/measurements/timeSeriesMeasurements/PercentOfChange.java
+++ src/main/java/com/farshad/algotrading/backtesting/measurements/timeSeriesMeasurements/PercentOfChange.java
@@ -5,4 +5,15 @@ import org.influxdb.annotation.Column;
 import org.influxdb.annotation.Measurement;
 
 @Measurement(name = "EURUSDChangeInPercent")
-p
+public class PercentOfChange extends TimeSeriesPoint {
+
+
+    @Column(name = "percent")
+    private double percent;
+
+
+    public double getPercent() {
+        return percent;
+    }
+
+    public void setPercent(double
