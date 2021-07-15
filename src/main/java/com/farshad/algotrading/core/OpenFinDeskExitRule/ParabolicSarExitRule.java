5c839eed8732ae4ec9814271811a6b9bce8483cb
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-07-15 15:49:00 +0000

    Automated merge with ssh://jfxsrc.us.oracle.com//javafx/8u/master/jfx/rt

--- src/main/java/com/farshad/algotrading/core/OpenFinDeskExitRule/ParabolicSarExitRule.java
+++ src/main/java/com/farshad/algotrading/core/OpenFinDeskExitRule/ParabolicSarExitRule.java
@@ -1,1 +1,7 @@
-package com.farshad.algotrading.core.Op
+package com.farshad.algotrading.core.OpenFinDeskExitRule;
+
+import org.ta4j.core.indicators.ParabolicSarIndicator;
+
+import static org.apache.commons.math3.util.Precision.round;
+
+public class ParabolicSarExitRule extends 
