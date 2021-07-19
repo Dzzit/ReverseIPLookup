98c4d875c29ba3d03e1387e0ad813047a59356a5
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-07-19 21:01:06 +0000

    Removed useless files

--- src/main/java/com/farshad/algotrading/core/OpenFinDeskExitRule/QuantizedTpAndSlExitRule.java
+++ src/main/java/com/farshad/algotrading/core/OpenFinDeskExitRule/QuantizedTpAndSlExitRule.java
@@ -5,4 +5,9 @@ import org.apache.log4j.Logger;
 
 import static org.apache.commons.math3.util.Precision.round;
 
-p
+public class QuantizedTpAndSlExitRule extends OpenFinDeskExitRule {
+    final static Logger logger= Logger.getLogger(QuantizedTpAndSlExitRule.class);
+
+    private String quantizedExitRuleAction;
+
+ 
