0123e3b2dd88e2f7235ced8ea93819e8f16c7244
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-08-07 09:31:29 +0000

    ALSA: hda - Add headphone quirk for Lifebook E752

--- src/main/java/com/farshad/algotrading/core/containerCore/XYSeriesDemo.java
+++ src/main/java/com/farshad/algotrading/core/containerCore/XYSeriesDemo.java
@@ -1,1 +1,7 @@
-package com.farshad.algotrading.core.con
+package com.farshad.algotrading.core.containerCore;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.ui.ApplicationFram
