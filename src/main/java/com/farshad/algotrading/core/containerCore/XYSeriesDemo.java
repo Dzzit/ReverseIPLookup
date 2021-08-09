5eacf5bacfa444a7e62c2e3fe34bad5494663e6c
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-08-09 10:29:02 +0000

    smd_pkt: Add sanity check to avoid unregistering the driver twice

--- src/main/java/com/farshad/algotrading/core/containerCore/XYSeriesDemo.java
+++ src/main/java/com/farshad/algotrading/core/containerCore/XYSeriesDemo.java
@@ -4,4 +4,10 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.ui.ApplicationFram
+import org.jfree.chart.ui.ApplicationFrame;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+public class XYSeriesDemo extends ApplicationFrame {
+
+    public XYSeries series = new XYSeries(
