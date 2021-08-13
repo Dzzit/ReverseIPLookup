2f1ed2f264e6318f63aab8f149601a9cb8875f26
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-08-13 22:47:03 +0000

    add ignore files

--- src/main/java/com/farshad/algotrading/core/fibonacciCore/Fibonacci.java
+++ src/main/java/com/farshad/algotrading/core/fibonacciCore/Fibonacci.java
@@ -2,4 +2,12 @@ package com.farshad.algotrading.core.fibonacciCore;
 
 import com.farshad.algotrading.core.trendRangeAndWave.Trend;
 import com.farshad.algotrading.core.trendRangeAndWave.Wave;
-import org.apache.log4j.Lo
+import org.apache.log4j.Logger;
+
+import java.util.*;
+
+public class Fibonacci {
+
+    final static Logger logger= Logger.getLogger(Fibonacci.class);
+
+    private List<Double> internalRetracementLevels=new ArrayLis
