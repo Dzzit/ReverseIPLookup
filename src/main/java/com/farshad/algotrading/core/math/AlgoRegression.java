095b67b445cecfe7bd22fc22b17fecf90ee3b72a
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-09-15 06:03:04 +0000

    Update edge-server.yml

--- src/main/java/com/farshad/algotrading/core/math/AlgoRegression.java
+++ src/main/java/com/farshad/algotrading/core/math/AlgoRegression.java
@@ -1,3 +1,14 @@
 package com.farshad.algotrading.core.math;
 
-import org.apache.commons.math3.stat.regression.Simpl
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+import org.apache.log4j.Logger;
+
+import java.util.List;
+
+public class AlgoRegression {
+
+    final static Logger logger= Logger.getLogger(AlgoRegression.class);
+
+
+    private double slope;
+   
