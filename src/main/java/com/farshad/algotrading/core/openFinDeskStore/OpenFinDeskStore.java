d4cb523bc0f81ed68159c1622ca37e11dabf7634
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-09-20 16:41:43 +0000

    Add src

--- src/main/java/com/farshad/algotrading/core/openFinDeskStore/OpenFinDeskStore.java
+++ src/main/java/com/farshad/algotrading/core/openFinDeskStore/OpenFinDeskStore.java
@@ -2,3 +2,10 @@ package com.farshad.algotrading.core.openFinDeskStore;
 
  import com.farshad.algotrading.core.sockets.SocketUtil;
 import org.ta4j.core.TimeSeries;
+
+
+public abstract class OpenFinDeskStore {
+
+    public abstract void initialize(String name,String timeFrame,String financialMarket,SocketUtil socketUtil);
+
+    public abstract void fetc
