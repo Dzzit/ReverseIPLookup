c90d3b9976dfa4dbab64b9e56918769fa2d5c363
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-09-20 19:40:17 +0000

    Run update-remote.sh on nextcloud

--- src/main/java/com/farshad/algotrading/core/openFinDeskStore/OpenFinDeskStore.java
+++ src/main/java/com/farshad/algotrading/core/openFinDeskStore/OpenFinDeskStore.java
@@ -8,4 +8,6 @@ public abstract class OpenFinDeskStore {
 
     public abstract void initialize(String name,String timeFrame,String financialMarket,SocketUtil socketUtil);
 
-    public abstract void fetc
+    public abstract void fetchCandlesFromMT5AndWriteItToOpenFinDeskStore(String symbolName,String timeFrame,int numberOfRequiredCandles,int count);
+
+    public abstract TimeSeries getSeries(String symbolName,String timeFrame,int numberOfLinesTore
