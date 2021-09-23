f5dc7bae288bf36647c91fca94fb52f22aa3d37b
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-09-23 15:42:03 +0000

    Code organization

--- src/main/java/com/farshad/algotrading/core/openFinDeskStore/csv/stocksInputData/OHLCData.java
+++ src/main/java/com/farshad/algotrading/core/openFinDeskStore/csv/stocksInputData/OHLCData.java
@@ -2,4 +2,16 @@ package com.farshad.algotrading.core.openFinDeskStore.csv.stocksInputData;
 
 public class OHLCData {
 
-    String
+    String symbol;
+    String time;
+    String open;
+    String high;
+    String low;
+    String close;
+    String tick_volume;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+ 
