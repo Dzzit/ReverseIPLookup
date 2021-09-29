3ad48eb8435c9fbcfa9bdc502d745195cbd26b67
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-09-29 10:16:36 +0000

    Add ChangeLog

--- src/main/java/com/farshad/algotrading/core/openFinDeskStore/csv/stocksInputData/OpenFinDeskMessage.java
+++ src/main/java/com/farshad/algotrading/core/openFinDeskStore/csv/stocksInputData/OpenFinDeskMessage.java
@@ -1,1 +1,10 @@
-package com.farshad.algotrading.
+package com.farshad.algotrading.core.openFinDeskStore.csv.stocksInputData;
+
+public class OpenFinDeskMessage {
+
+    private String context;
+
+    private String payload;
+
+    public String getContext() {
+        return co
