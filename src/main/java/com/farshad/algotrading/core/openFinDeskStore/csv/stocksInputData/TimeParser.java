cfa67e67472d39180f171ae9c2bcc88e9e1cb52c
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-09-30 14:01:53 +0000

    net: core: Always propagate flag changes to interfaces

--- src/main/java/com/farshad/algotrading/core/openFinDeskStore/csv/stocksInputData/TimeParser.java
+++ src/main/java/com/farshad/algotrading/core/openFinDeskStore/csv/stocksInputData/TimeParser.java
@@ -24,3 +24,49 @@ public class TimeParser {
          minute=Integer.parseInt(myTime.split(":")[1]);
 
          year=Integer.parseInt(myDate.split(Pattern.quote("."))[0].replaceAll("^[0]+", ""));
+         month=Integer.parseInt(myDate.split(Pattern.quote("."))[1].replaceAll("^[0]+", ""));
+         dates=Integer.parseInt(myDate.split(Pattern.quote("."))[2].replaceAll("^[0]+", ""));
+
+    }
+
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDates() {
+        return dates;
+    }
+
+    public void setDates(int dates) {
+        this.dates = dates;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+}
---
+++ src/main/java/com/farshad/algotrading/core/openFinDeskStore/csv/stocksInputData/csv/CSVReader.java
@@ -0,0 +1,10 @@
+package com.farshad.algotrading.core.openFinDeskStore.csv.stocksInputData.csv;
+
+
+import java.io.*;
+
+
+
+public class CSVReader {
+
+    private Buffere
