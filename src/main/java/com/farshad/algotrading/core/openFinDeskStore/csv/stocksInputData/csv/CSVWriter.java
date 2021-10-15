a052b1b535b729760e8b627535d8bb71e0a468bf
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-10-15 17:45:32 +0000

    Create index.htm

--- src/main/java/com/farshad/algotrading/core/openFinDeskStore/csv/stocksInputData/csv/CSVWriter.java
+++ src/main/java/com/farshad/algotrading/core/openFinDeskStore/csv/stocksInputData/csv/CSVWriter.java
@@ -1,4 +1,13 @@
 package com.farshad.algotrading.core.openFinDeskStore.csv.stocksInputData.csv;
 
 
-im
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CSVWriter {
+
+    private static Logger LOGGER= Logg
