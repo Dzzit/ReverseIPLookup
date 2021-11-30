94980240e660c65253ab42afe4344d735365dbca
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-11-30 06:08:45 +0000

    More build fixes.

--- src/main/java/com/farshad/algotrading/core/sockets/SocketUtil.java
+++ src/main/java/com/farshad/algotrading/core/sockets/SocketUtil.java
@@ -4,4 +4,12 @@ import com.farshad.algotrading.OpenFinDesklogLevels.LiveTradingLogLevel;
 import com.google.gson.Gson;
 import com.farshad.algotrading.core.openFinDeskStore.csv.stocksInputData.OHLCData;
 import com.farshad.algotrading.core.openFinDeskStore.csv.stocksInputData.OpenFinDeskMessage;
-imp
+import org.apache.log4j.Logger;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.ne
