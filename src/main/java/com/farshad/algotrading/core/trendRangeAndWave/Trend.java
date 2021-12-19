7ed576731394083a8c0292ea41813f881ca6943b
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-12-19 09:16:36 +0000

    Update orthography.tsv

--- src/main/java/com/farshad/algotrading/core/trendRangeAndWave/Trend.java
+++ src/main/java/com/farshad/algotrading/core/trendRangeAndWave/Trend.java
@@ -1,3 +1,13 @@
 package com.farshad.algotrading.core.trendRangeAndWave;
 
-import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements
+import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.PriceTime;
+import org.apache.log4j.Logger;
+
+import java.util.List;
+
+/**
+ * @author farshad noravesh
+ * @since version 1.0.0
+ */
+public class Trend {
+    final static Logger logger= Logger
