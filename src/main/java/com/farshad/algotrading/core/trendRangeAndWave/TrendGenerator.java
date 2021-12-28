ee556b106b3ac0010e6236ba83fb549da5de9377
Dzzit <93486011+Dzzit@users.noreply.github.com>
2021-12-28 17:56:09 +0000

    travis.

--- src/main/java/com/farshad/algotrading/core/trendRangeAndWave/TrendGenerator.java
+++ src/main/java/com/farshad/algotrading/core/trendRangeAndWave/TrendGenerator.java
@@ -1,3 +1,10 @@
 package com.farshad.algotrading.core.trendRangeAndWave;
 
-import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.Pr
+import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.PriceTime;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrendGenerator {
+    final static Logger logger= Logger.getLogger(TrendGenerator.clas
