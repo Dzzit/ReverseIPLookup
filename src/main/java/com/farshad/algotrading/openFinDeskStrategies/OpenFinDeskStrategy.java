af126844e815c3798e84f9f2fc54c9dbea7b4df2
Dzzit <93486011+Dzzit@users.noreply.github.com>
2022-01-08 22:52:37 +0000

    update debian/changelog

--- src/main/java/com/farshad/algotrading/openFinDeskStrategies/OpenFinDeskStrategy.java
+++ src/main/java/com/farshad/algotrading/openFinDeskStrategies/OpenFinDeskStrategy.java
@@ -1,1 +1,10 @@
-package com.farshad.algotrading.openFinDesk
+package com.farshad.algotrading.openFinDeskStrategies;
+
+import com.farshad.algotrading.core.openFinDeskOrder.OpenFinDeskOrder;
+import org.ta4j.core.*;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+/**
+ * @author fa
