11f2cf34ca45e92ae650e7e3a2267accbc67fe9b
Dzzit <93486011+Dzzit@users.noreply.github.com>
2022-01-19 05:33:20 +0000

    add another example

--- src/main/java/com/farshad/algotrading/openFinDeskStrategies/candlestickDriven/PinBarStrategy.java
+++ src/main/java/com/farshad/algotrading/openFinDeskStrategies/candlestickDriven/PinBarStrategy.java
@@ -1,3 +1,8 @@
 package com.farshad.algotrading.openFinDeskStrategies.candlestickDriven;
 
-import com.farshad.algotrading.core.
+import com.farshad.algotrading.core.openFinDeskOrder.OpenFinDeskOrder;
+import com.farshad.algotrading.openFinDeskStrategies.OpenFinDeskStrategy;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import 
