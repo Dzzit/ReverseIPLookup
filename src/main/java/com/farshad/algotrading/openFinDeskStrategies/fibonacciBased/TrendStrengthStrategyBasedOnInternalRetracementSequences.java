9e30d9c8f563ea0e75c651dcee2437368ad7cb61
Dzzit <93486011+Dzzit@users.noreply.github.com>
2022-01-27 17:15:29 +0000

    Formatting and comments

--- src/main/java/com/farshad/algotrading/openFinDeskStrategies/fibonacciBased/TrendStrengthStrategyBasedOnInternalRetracementSequences.java
+++ src/main/java/com/farshad/algotrading/openFinDeskStrategies/fibonacciBased/TrendStrengthStrategyBasedOnInternalRetracementSequences.java
@@ -2,4 +2,6 @@ package com.farshad.algotrading.openFinDeskStrategies.fibonacciBased;
 
 import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.PriceTime;
 import com.farshad.algotrading.core.AlternatingTrendSmoothing.ATS;
-impo
+import com.farshad.algotrading.core.openFinDeskOrder.OpenFinDeskOrder;
+import com.farshad.algotrading.core.fibonacciCore.FibonacciHelper;
+import com.farshad.algotrading.core.trendRangeAndWa
