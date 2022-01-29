4a022a2a6f2cc649f2fa0a572efe81f4f59e9df9
Dzzit <93486011+Dzzit@users.noreply.github.com>
2022-01-29 08:04:12 +0000

    cpufreq: interactive: trace actual speed in target speed decisions

--- src/main/java/com/farshad/algotrading/openFinDeskStrategies/fibonacciBased/TrendStrengthStrategyBasedOnInternalRetracementSequences.java
+++ src/main/java/com/farshad/algotrading/openFinDeskStrategies/fibonacciBased/TrendStrengthStrategyBasedOnInternalRetracementSequences.java
@@ -4,4 +4,7 @@ import com.farshad.algotrading.backtesting.measurements.timeSeriesMeasurements.P
 import com.farshad.algotrading.core.AlternatingTrendSmoothing.ATS;
 import com.farshad.algotrading.core.openFinDeskOrder.OpenFinDeskOrder;
 import com.farshad.algotrading.core.fibonacciCore.FibonacciHelper;
-import com.farshad.algotrading.core.trendRangeAndWa
+import com.farshad.algotrading.core.trendRangeAndWave.Trend;
+import com.farshad.algotrading.core.trendRangeAndWave.TrendGenerator;
+import com.farshad.algotrading.core.trendRangeAndWave.Wave;
+import com.farshad.algotrading.core.trendRangeAndWave.WaveGenerato
