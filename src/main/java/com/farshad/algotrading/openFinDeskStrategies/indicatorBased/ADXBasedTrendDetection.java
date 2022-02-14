79b354008956c0c0bff306da5a33665753cb3fac
Dzzit <93486011+Dzzit@users.noreply.github.com>
2022-02-14 21:44:22 +0000

    Update bot.php

--- src/main/java/com/farshad/algotrading/openFinDeskStrategies/indicatorBased/ADXBasedTrendDetection.java
+++ src/main/java/com/farshad/algotrading/openFinDeskStrategies/indicatorBased/ADXBasedTrendDetection.java
@@ -2,4 +2,8 @@ package com.farshad.algotrading.openFinDeskStrategies.indicatorBased;
 
 import com.farshad.algotrading.core.openFinDeskOrder.OpenFinDeskOrder;
 import com.farshad.algotrading.openFinDeskStrategies.CrossingTimeSeriesDetector;
-import com.farshad.algotrading.openFinDeskStrategies.OpenFinDeskSt
+import com.farshad.algotrading.openFinDeskStrategies.OpenFinDeskStrategy;
+import org.apache.log4j.Logger;
+import org.ta4j.core.indicators.ParabolicSarIndicator;
+import org.ta4j.core.indicators.adx.ADXIndicator;
+import org.ta4j.core.indicators.adx.MinusDIIndica
