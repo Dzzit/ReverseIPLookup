51cec648c39967c53ee04d5eabd22cfa5d6922bb
Dzzit <93486011+Dzzit@users.noreply.github.com>
2022-03-30 15:56:52 +0000

    ysq

--- src/main/java/com/farshad/algotrading/openFinDeskStrategies/indicatorBased/MACDbasedCrossingStrategy.java
+++ src/main/java/com/farshad/algotrading/openFinDeskStrategies/indicatorBased/MACDbasedCrossingStrategy.java
@@ -3,4 +3,9 @@ package com.farshad.algotrading.openFinDeskStrategies.indicatorBased;
 import com.farshad.algotrading.core.openFinDeskOrder.OpenFinDeskOrder;
 import com.farshad.algotrading.openFinDeskStrategies.CrossingTimeSeriesDetector;
 import com.farshad.algotrading.openFinDeskStrategies.OpenFinDeskStrategy;
-import org.ta4j.core.ind
+import org.ta4j.core.indicators.EMAIndicator;
+import org.ta4j.core.indicators.MACDIndicator;
+import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
+
+import java.io.IOException;
+import java.util.co
