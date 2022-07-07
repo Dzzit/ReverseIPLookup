2feddf7adcece80300c83e8143023e5f13e115c9
Dzzit <93486011+Dzzit@users.noreply.github.com>
2022-07-07 10:49:57 +0000

    recommit

--- src/main/java/com/farshad/algotrading/openFinDeskStrategies/statisticalArbitrageBasedOnKalmanFilter/Cointegration.java
+++ src/main/java/com/farshad/algotrading/openFinDeskStrategies/statisticalArbitrageBasedOnKalmanFilter/Cointegration.java
@@ -1,4 +1,13 @@
 package com.farshad.algotrading.openFinDeskStrategies.statisticalArbitrageBasedOnKalmanFilter;
 
 
-import org.la4j
+import org.la4j.Matrix;
+
+public class Cointegration {
+    double mDelta;
+    double mR;
+    KalmanFilter mFilter;
+    int mNobs = 2;
+
+    public Cointegration(double delta, double r) {
+        mDe
