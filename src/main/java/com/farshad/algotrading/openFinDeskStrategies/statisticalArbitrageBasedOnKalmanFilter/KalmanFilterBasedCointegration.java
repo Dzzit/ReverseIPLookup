2d0b9a726a037c282b4a1a3e28b7380f3502266f
Dzzit <93486011+Dzzit@users.noreply.github.com>
2022-08-15 20:28:58 +0000

    add image

--- src/main/java/com/farshad/algotrading/openFinDeskStrategies/statisticalArbitrageBasedOnKalmanFilter/KalmanFilterBasedCointegration.java
+++ src/main/java/com/farshad/algotrading/openFinDeskStrategies/statisticalArbitrageBasedOnKalmanFilter/KalmanFilterBasedCointegration.java
@@ -2,4 +2,8 @@
 package com.farshad.algotrading.openFinDeskStrategies.statisticalArbitrageBasedOnKalmanFilter;
 
 import com.farshad.algotrading.containerCore.openFinDeskOrder.OpenFinDeskOrder;
-import com.farshad.algotrading.openFinDesk
+import com.farshad.algotrading.openFinDeskStrategies.OpenFinDeskStrategy;
+import org.ta4j.com.farshad.algotrading.containerCore.TimeSeriesPoint;
+import org.ta4j.com.farshad.algotrading.containerCore.TradingRecord;
+
+import java.io.IOEx
