26b1c45d785927b1cae3817664809ec538509a2a
Dzzit <93486011+Dzzit@users.noreply.github.com>
2022-09-10 15:15:05 +0000

    reduce image size

--- src/main/java/com/farshad/algotrading/openFinDeskStrategies/statisticalArbitrageBasedOnKalmanFilter/Process.java
+++ src/main/java/com/farshad/algotrading/openFinDeskStrategies/statisticalArbitrageBasedOnKalmanFilter/Process.java
@@ -2,4 +2,8 @@
 package com.farshad.algotrading.openFinDeskStrategies.statisticalArbitrageBasedOnKalmanFilter;
 
 import org.apache.log4j.Logger;
-import org.t
+import org.ta4j.com.farshad.algotrading.containerCore.TimeSeriesPoint;
+import org.ta4j.com.farshad.algotrading.containerCore.TradingRecord;
+import com.farshad.algotrading.containerCore.Symbol;
+
+import java.io.IOEx
