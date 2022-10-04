98da61635e1aeea3e5fd5d832d1329a1d201e96f
Dzzit <93486011+Dzzit@users.noreply.github.com>
2022-10-04 13:22:55 +0000

    add fedora_31, fedora_32, ubi8_8.0 CI builds where appropriate

--- src/main/java/com/farshad/algotrading/strategyContainer/container1/CheckIfWeHavePinBarCandlestickChanceNode.java
+++ src/main/java/com/farshad/algotrading/strategyContainer/container1/CheckIfWeHavePinBarCandlestickChanceNode.java
@@ -1,1 +1,5 @@
-package com.farshad.algotrading.strategyContainer.container1
+package com.farshad.algotrading.strategyContainer.container1;
+
+import com.farshad.algotrading.openFinDeskAnnotations.OpenFinDeskChanceNode;
+import com.farshad.algotrading.openFinDeskStrategies.candlestickDriven.PinBarStrategy;
+import com.farshad.algotradi
