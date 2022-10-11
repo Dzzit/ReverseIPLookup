d1a667d0908381ce898751b9bc31a81c482eaa1e
Dzzit <93486011+Dzzit@users.noreply.github.com>
2022-10-11 10:52:50 +0000

    oh my, I hate roselyn

--- src/main/java/com/farshad/algotrading/strategyContainer/container1/CheckIfWeHavePinBarCandlestickChanceNode.java
+++ src/main/java/com/farshad/algotrading/strategyContainer/container1/CheckIfWeHavePinBarCandlestickChanceNode.java
@@ -8,4 +8,6 @@ import com.farshad.algotrading.core.containerCore.ChanceNode;
 
 @OpenFinDeskChanceNode(containerId=1,nodeId=0
 ,openfindeskStrategies={PinBarStrategy.class,TrendStrengthStrategyBasedOnInternalRetracementSequences.class}
-,timeFra
+,timeFrames={"PERIOD_H1","PERIOD_D1"},disabled=true)
+public class CheckIfWeHavePinBarCandlestickChanceNode extends ChanceNode {
+    final static Logger logger= Logger.getLogger(CheckIfWeHave
