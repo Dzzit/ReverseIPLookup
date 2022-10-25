f762fb876c3f46032b69dbf04dff88cdc1f8d342
Dzzit <93486011+Dzzit@users.noreply.github.com>
2022-10-25 22:07:41 +0000

    Fixed issue.

--- src/main/java/com/farshad/algotrading/strategyContainer/container2/CheckIfCrossingMACDAndTrendingADXChanceNode.java
+++ src/main/java/com/farshad/algotrading/strategyContainer/container2/CheckIfCrossingMACDAndTrendingADXChanceNode.java
@@ -2,4 +2,9 @@ package com.farshad.algotrading.strategyContainer.container2;
 
 import com.farshad.algotrading.openFinDeskAnnotations.OpenFinDeskChanceNode;
 import com.farshad.algotrading.openFinDeskStrategies.indicatorBased.ADXBasedTrendDetection;
-import com.farshad.algotradi
+import com.farshad.algotrading.openFinDeskStrategies.indicatorBased.MACDbasedCrossingStrategy;
+import com.farshad.algotrading.core.containerCore.ChanceNode;
+import org.apache.log4j.Logger;
+
+@OpenFinDeskChanceNode(containerId=2,nodeId=0
+  
