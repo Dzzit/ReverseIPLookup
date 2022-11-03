ba234cfbedc4ee51fd3934a49ddb5ecbd67766ea
Dzzit <93486011+Dzzit@users.noreply.github.com>
2022-11-03 22:34:32 +0000

    Pontoon: Update Portuguese (pt-BR) localization of Thimble

--- src/main/java/com/farshad/algotrading/strategyContainer/container3/CheckIfCrossingMovingAverageChanceNode.java
+++ src/main/java/com/farshad/algotrading/strategyContainer/container3/CheckIfCrossingMovingAverageChanceNode.java
@@ -4,4 +4,9 @@ package com.farshad.algotrading.strategyContainer.container3;
 import com.farshad.algotrading.openFinDeskAnnotations.OpenFinDeskChanceNode;
 import com.farshad.algotrading.openFinDeskStrategies.indicatorBased.ADXBasedTrendDetection;
 import com.farshad.algotrading.openFinDeskStrategies.indicatorBased.CrossingTwoMovingAverages;
-import com.farshad.algotrading.core.containerCore.Chanc
+import com.farshad.algotrading.core.containerCore.ChanceNode;
+import org.apache.log4j.Logger;
+
+@OpenFinDeskChanceNode(containerId=3,nodeId=0
+        ,openfindeskStrategies={CrossingTwoMovingAverages.class,ADXBasedTrendDetection.class}
+        ,ti
