5bceb33ec095be71d433947d2c9c0a85138af403
Dzzit <93486011+Dzzit@users.noreply.github.com>
2022-11-07 09:54:15 +0000

    Add placeholder

--- src/main/java/com/farshad/algotrading/strategyContainer/container3/CheckIfCrossingMovingAverageChanceNode.java
+++ src/main/java/com/farshad/algotrading/strategyContainer/container3/CheckIfCrossingMovingAverageChanceNode.java
@@ -9,4 +9,6 @@ import org.apache.log4j.Logger;
 
 @OpenFinDeskChanceNode(containerId=3,nodeId=0
         ,openfindeskStrategies={CrossingTwoMovingAverages.class,ADXBasedTrendDetection.class}
-        ,ti
+        ,timeFrames={"PERIOD_H1","PERIOD_D1"},disabled=true)
+public class CheckIfCrossingMovingAverageChanceNode extends ChanceNode {
+    final static Logger logger= Logger.getLogger(CheckIf
