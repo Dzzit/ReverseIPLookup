954c0257475f408e8c89a3d3465b6db25282f781
Dzzit <93486011+Dzzit@users.noreply.github.com>
2022-12-22 16:10:09 +0000

    remove unnecessary tests

--- src/main/java/com/farshad/algotrading/strategyContainer/container9/CheckIfBothTimeFramesAgreeChanceNode.java
+++ src/main/java/com/farshad/algotrading/strategyContainer/container9/CheckIfBothTimeFramesAgreeChanceNode.java
@@ -2,4 +2,8 @@
 package com.farshad.algotrading.strategyContainer.container9;
 
  import com.farshad.algotrading.openFinDeskAnnotations.OpenFinDeskChanceNode;
- import com.farshad.algotrading.openFinDeskStrategies.fibonacciBased.T
+ import com.farshad.algotrading.openFinDeskStrategies.fibonacciBased.TrendStrengthStrategyBasedOnInternalRetracementSequences;
+ import org.apache.log4j.Logger;
+ import com.farshad.algotrading.core.containerCore.ChanceNode;
+
+ @OpenFinDeskChanceNode(containerId=9,no
