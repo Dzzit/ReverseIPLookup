e5ad486ea40a95ffe1aaae76ae2c388b81b97624
Dzzit <93486011+Dzzit@users.noreply.github.com>
2023-02-16 08:04:41 +0000

    development update

--- src/test/java/com/farshad/algotrading/TestCSVReader.java
+++ src/test/java/com/farshad/algotrading/TestCSVReader.java
@@ -1,4 +1,12 @@
 package com.farshad.algotrading;
 
 import com.farshad.algotrading.core.openFinDeskStore.csv.stocksInputData.csv.CSVReader;
-import org.
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TestCSVReader {
+    final static Logger logger= Logger.getLogger(TestCSVReader.class);
+
+    public static
