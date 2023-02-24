2610acbfb04579bae30cf0a89fc13c7305188237
Dzzit <93486011+Dzzit@users.noreply.github.com>
2023-02-24 13:49:46 +0000

    Rolled version number

--- src/test/java/com/farshad/algotrading/TestRegressionAndPredictForex.java
+++ src/test/java/com/farshad/algotrading/TestRegressionAndPredictForex.java
@@ -2,4 +2,9 @@ package com.farshad.algotrading;
 
 import org.apache.log4j.Logger;
 
-public class TestRegressionAnd
+public class TestRegressionAndPredictForex {
+    final static Logger logger= Logger.getLogger(TestRegressionAndPredictForex.class);
+
+    public static void main(String[] args) {
+        double EURUSD=1.10813;
+        double GBPUSD=0.02773169
