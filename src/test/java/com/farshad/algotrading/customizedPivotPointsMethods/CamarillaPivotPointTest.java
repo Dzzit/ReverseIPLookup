5becde9106ffe7f6879ebcd694f423aced744e65
Dzzit <93486011+Dzzit@users.noreply.github.com>
2023-02-24 17:58:39 +0000

    Renamed test file

--- src/test/java/com/farshad/algotrading/customizedPivotPointsMethods/CamarillaPivotPointTest.java
+++ src/test/java/com/farshad/algotrading/customizedPivotPointsMethods/CamarillaPivotPointTest.java
@@ -1,3 +1,7 @@
 package com.farshad.algotrading.customizedPivotPointsMethods;
 
-import com.farshad.algotrading
+import com.farshad.algotrading.core.customizedPivotPointMethods.CamarillaPivotPointApproach;
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.juni
